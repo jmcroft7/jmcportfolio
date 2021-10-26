fb6ce94ef0d10e4e553039632ed8ebf28d02ae57
Johnathan Croft <croftmjohn@gmail.com>
2021-10-26 15:52:07 -0500

    corrected link routes

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
 
-    const progressBar = document.querySelector("#progressBar");
-    let totalPageHeight = document.body.scrollHeight - window.innerHeight;
-    window.onscroll = () => {
-        let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
-        progressBar.style.height = `${newProgressHeight}%`;
-        progressBar.style.opacity = `${newProgressHeight}%`;
-    };
-
     $(window).scroll(function () {
         // sticky navbar on scroll script
         if (this.scrollY > 20) {
